16ac36429ab2d3faf199bc89964f37b368035d08
dhruv918 <83133130+dhruv918@users.noreply.github.com>
2021-06-10 16:01:18 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,5 @@
+import os
+import shutil
+source="C:/Users/Sandeep/Desktop/whitehatjr/New folder (5)/source/text.txt"
+destination="C:/Users/Sandeep/Desktop/whitehatjr/New folder (5)/destination"
+dest=shutil.move(source,destination)
